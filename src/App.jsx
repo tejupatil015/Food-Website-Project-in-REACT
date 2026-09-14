04ac1be45f84ad9d318a9d83d54d7ceeae582e2b
tejupatil015 <tejupatil1506@gmail.com>
2026-09-14 19:11:02 +0530

    Initial React food delivery project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>Food Delivery App</h1>
+      <p>My React project is working!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Foodie</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
